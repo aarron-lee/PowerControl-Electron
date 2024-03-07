112e53b2c57a29668cbd0a975eed29abd100a095
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-07 12:25:36 -0500

    init vite react ts + cleanup components

--- src/util/backend.ts
+++ src/util/backend.ts
@@ -1,101 +1,110 @@
-import {ServerAPI } from "decky-frontend-lib";
 import { APPLYTYPE, FANMODE } from "./enum";
 import { FanControl } from "./pluginMain";
 import { Settings } from "./settings";
 
-
-
-export class BackendData{
-  private serverAPI:ServerAPI | undefined;
+export class BackendData {
+  private serverAPI: any;
   private fanMaxRPM = 0;
   private has_fanMaxRPM = false;
   private fanIsAdapted = false;
-  public async init(serverAPI:ServerAPI){
-    this.serverAPI=serverAPI;
+  public async init(serverAPI: any) {
+    this.serverAPI = serverAPI;
 
-    await this.serverAPI!.callPluginMethod<{},number>("get_fanMAXRPM",{}).then(res=>{
-      if (res.success){
-        this.fanMaxRPM=res.result;
-        this.has_fanMaxRPM=true;
-      }else{
-        this.fanMaxRPM=1;
+    await this.serverAPI!.callPluginMethod<{}, number>(
+      "get_fanMAXRPM",
+      {}
+    ).then((res) => {
+      if (res.success) {
+        this.fanMaxRPM = res.result;
+        this.has_fanMaxRPM = true;
+      } else {
+        this.fanMaxRPM = 1;
       }
-    })
-    await this.serverAPI!.callPluginMethod<{},boolean>("get_fanIsAdapted",{}).then(res=>{
-      if (res.success){
-        this.fanIsAdapted=res.result;
-      }else{
-        this.fanIsAdapted=false;
+    });
+    await this.serverAPI!.callPluginMethod<{}, boolean>(
+      "get_fanIsAdapted",
+      {}
+    ).then((res) => {
+      if (res.success) {
+        this.fanIsAdapted = res.result;
+      } else {
+        this.fanIsAdapted = false;
       }
-    })
+    });
   }
 
-  public getFanMAXPRM(){
+  public getFanMAXPRM() {
     return this.fanMaxRPM;
   }
 
-  public HasFanMAXPRM(){
+  public HasFanMAXPRM() {
     return this.has_fanMaxRPM;
   }
 
-  public getFanIsAdapt(){
+  public getFanIsAdapt() {
     return this.fanIsAdapted;
   }
 
-  public async getFanRPM(){
-    var fanPRM:number;
-    await this.serverAPI!.callPluginMethod<{},number>("get_fanRPM",{}).then(res=>{
-      if (res.success){
-        fanPRM=res.result;
-      }else{
-        fanPRM=0;
+  public async getFanRPM() {
+    var fanPRM: number;
+    await this.serverAPI!.callPluginMethod<{}, number>("get_fanRPM", {}).then(
+      (res) => {
+        if (res.success) {
+          fanPRM = res.result;
+        } else {
+          fanPRM = 0;
+        }
       }
-    })
+    );
     return fanPRM!!;
   }
 
-  public async getFanTemp(){
-    var fanTemp:number;
-    await this.serverAPI!.callPluginMethod<{},number>("get_fanTemp",{}).then(res=>{
-      if (res.success){
-        fanTemp=res.result/1000;
-      }else{
-        fanTemp=-1;
+  public async getFanTemp() {
+    var fanTemp: number;
+    await this.serverAPI!.callPluginMethod<{}, number>("get_fanTemp", {}).then(
+      (res) => {
+        if (res.success) {
+          fanTemp = res.result / 1000;
+        } else {
+          fanTemp = -1;
+        }
       }
-    })
+    );
     return fanTemp!!;
   }
 
-  public async getFanIsAuto(){
-    var fanIsAuto:boolean;
-    await this.serverAPI!.callPluginMethod<{},boolean>("get_fanIsAuto",{}).then(res=>{
-      if (res.success){
-        fanIsAuto=res.result;
-      }else{
-        fanIsAuto=false;
+  public async getFanIsAuto() {
+    var fanIsAuto: boolean;
+    await this.serverAPI!.callPluginMethod<{}, boolean>(
+      "get_fanIsAuto",
+      {}
+    ).then((res) => {
+      if (res.success) {
+        fanIsAuto = res.result;
+      } else {
+        fanIsAuto = false;
       }
-    })
+    });
     return fanIsAuto!!;
   }
 }
 
-
 export class Backend {
-  private static serverAPI: ServerAPI;
+  private static serverAPI: any;
   public static data: BackendData;
-  public static async init(serverAPI: ServerAPI) {
+  public static async init(serverAPI: any) {
     this.serverAPI = serverAPI;
     this.data = new BackendData();
     await this.data.init(serverAPI);
   }
 
-  private static applyFanAuto(auto:boolean){
-    this.serverAPI!.callPluginMethod("set_fanAuto", {"value":auto});
+  private static applyFanAuto(auto: boolean) {
+    this.serverAPI!.callPluginMethod("set_fanAuto", { value: auto });
   }
-  private static applyFanPercent(percent:number){
-    this.serverAPI!.callPluginMethod("set_fanPercent", {"value":percent});
+  private static applyFanPercent(percent: number) {
+    this.serverAPI!.callPluginMethod("set_fanPercent", { value: percent });
   }
-  public static throwSuspendEvt(){
+  public static throwSuspendEvt() {
     console.log("throwSuspendEvt");
     this.serverAPI!.callPluginMethod("receive_suspendEvent", {});
   }
@@ -106,25 +115,31 @@ export class Backend {
       return;
     }
 
-    if (applyTarget == APPLYTYPE.SET_ALL || applyTarget == APPLYTYPE.SET_FANMODE){
-      if(!FanControl.fanIsEnable){
+    if (
+      applyTarget == APPLYTYPE.SET_ALL ||
+      applyTarget == APPLYTYPE.SET_FANMODE
+    ) {
+      if (!FanControl.fanIsEnable) {
         return;
       }
       const fanSetting = Settings.appFanSetting();
       const fanMode = fanSetting?.fanMode;
       if (fanMode == FANMODE.NOCONTROL) {
-          Backend.applyFanAuto(true);
+        Backend.applyFanAuto(true);
       } else if (fanMode == FANMODE.FIX) {
         Backend.applyFanAuto(false);
       } else if (fanMode == FANMODE.CURVE) {
         Backend.applyFanAuto(false);
       } else {
-          Backend.applyFanAuto(true);
-          console.log(`出现意外的FanMode = ${fanMode}`)
-      };
+        Backend.applyFanAuto(true);
+        console.log(`出现意外的FanMode = ${fanMode}`);
+      }
     }
-    if (applyTarget == APPLYTYPE.SET_ALL || applyTarget == APPLYTYPE.SET_FANRPM){
-      if(!FanControl.fanIsEnable){
+    if (
+      applyTarget == APPLYTYPE.SET_ALL ||
+      applyTarget == APPLYTYPE.SET_FANRPM
+    ) {
+      if (!FanControl.fanIsEnable) {
         return;
       }
       const fanSetting = Settings.appFanSetting();
@@ -135,14 +150,14 @@ export class Backend {
       } else if (fanMode == FANMODE.CURVE) {
         Backend.applyFanPercent(FanControl.setPoint.fanRPMpercent!!);
       } else {
-        console.log(`出现意外的FanMode = ${fanMode}`)
+        console.log(`出现意外的FanMode = ${fanMode}`);
       }
     }
   };
 
-  public static resetFanSettings = () =>{
+  public static resetFanSettings = () => {
     Backend.applyFanAuto(true);
-  }
+  };
 
   public static resetSettings = () => {
     console.log("重置所有设置");
--- src/util/pluginMain.ts
+++ src/util/pluginMain.ts
@@ -1,238 +1,228 @@
-import { Router, ServerAPI } from "decky-frontend-lib";
-import { APPLYTYPE, ComponentName, FANMODE, PluginState, UpdateType } from "./enum";
-import { Backend} from "./backend";
+import {
+  APPLYTYPE,
+  ComponentName,
+  FANMODE,
+  PluginState,
+  UpdateType,
+} from "./enum";
+import { Backend } from "./backend";
 import { localizationManager } from "../i18n";
 import { Settings } from "./settings";
-import { AppOverviewExt } from "./steamClient";
 import { calPointInLine, fanPosition } from "./position";
+import serverAPI from "./serverAPI";
 
-type ActiveAppChangedHandler = (newAppId: string, oldAppId: string) => void;
-type ComponentUpdateHandler = (componentsName: ComponentName,updateType:UpdateType) => void;
+type ComponentUpdateHandler = (
+  componentsName: ComponentName,
+  updateType: UpdateType
+) => void;
 type UnregisterFn = () => void;
-export const DEFAULT_APP = "0";
-export class RunningApps {
-    private static listeners: ActiveAppChangedHandler[] = [];
-    private static lastAppId: string = DEFAULT_APP;
-    private static intervalId: any;
-  
-    private static pollActive() {
-      const newApp = RunningApps.active();
-      if (this.lastAppId != newApp) {
-        this.listeners.forEach((h) => h(newApp, this.lastAppId));
-      }
-      this.lastAppId = newApp;
-    }
-  
-    static register() {
-      if (this.intervalId == undefined)
-        this.intervalId = setInterval(() => this.pollActive(), 100);
-    }
-  
-    static unregister() {
-      if (this.intervalId != undefined)
-        clearInterval(this.intervalId);
-  
-      this.listeners.splice(0, this.listeners.length);
-    }
-  
-    static listenActiveChange(fn: ActiveAppChangedHandler): UnregisterFn {
-      const idx = this.listeners.push(fn) - 1;
-      return () => {
-        this.listeners.splice(idx, 1);
-      };
-    }
-  
-    static active() {
-      return Router.MainRunningApp?.appid || DEFAULT_APP;
-    }
-  
-    static active_appInfo() {
-      return Router.MainRunningApp as unknown as AppOverviewExt || null;
-    }
-}
 
-export class FanControl{
+export class FanControl {
   private static intervalId: any;
-  public static nowPoint:fanPosition=new fanPosition(0,0);
-  public static setPoint:fanPosition=new fanPosition(0,0);
-  public static fanMode:FANMODE;
-  public static fanRPM:number=0;
-  public static fanIsEnable:boolean=false;
+  public static nowPoint: fanPosition = new fanPosition(0, 0);
+  public static setPoint: fanPosition = new fanPosition(0, 0);
+  public static fanMode: FANMODE;
+  public static fanRPM: number = 0;
+  public static fanIsEnable: boolean = false;
   static async register() {
-    if(!Backend.data.getFanIsAdapt()){
+    if (!Backend.data.getFanIsAdapt()) {
       this.disableFan();
       return;
     }
     if (this.intervalId == undefined)
       this.intervalId = setInterval(() => this.updateFan(), 1000);
-    this.fanIsEnable=true;
-  }
-  
-  static async updateFan(){
-    FanControl.updateFanMode();
-    FanControl.updateFanInfo();
-    PluginManager.updateComponent(ComponentName.FAN_DISPLAY,UpdateType.UPDATE);
+    this.fanIsEnable = true;
   }
 
-  static async updateFanMode(){
+  static async updateFan() {
+    FanControl.updateFanMode();
+    FanControl.updateFanInfo();
+    PluginManager.updateComponent(ComponentName.FAN_DISPLAY, UpdateType.UPDATE);
+  }
+
+  static async updateFanMode() {
     const fanSetting = Settings.appFanSetting();
     const fanMode = fanSetting?.fanMode;
-    if(FanControl.fanMode==undefined||FanControl.fanMode!=fanMode)
-    {
+    if (FanControl.fanMode == undefined || FanControl.fanMode != fanMode) {
       FanControl.fanMode = fanMode!!;
       Backend.applySettings(APPLYTYPE.SET_FANMODE);
     }
   }
 
-  static async updateFanInfo(){
-    await Backend.data.getFanRPM().then((value)=>{
-      this.fanRPM=value;
-      FanControl.nowPoint.fanRPMpercent=Backend.data.HasFanMAXPRM()?value/Backend.data.getFanMAXPRM()*100:-273;
+  static async updateFanInfo() {
+    await Backend.data.getFanRPM().then((value) => {
+      this.fanRPM = value;
+      FanControl.nowPoint.fanRPMpercent = Backend.data.HasFanMAXPRM()
+        ? (value / Backend.data.getFanMAXPRM()) * 100
+        : -273;
     });
-    await Backend.data.getFanTemp().then((value)=>{
-      FanControl.nowPoint.temperature=value;
+    await Backend.data.getFanTemp().then((value) => {
+      FanControl.nowPoint.temperature = value;
     });
     const fanSetting = Settings.appFanSetting();
     const fanMode = fanSetting?.fanMode;
-    switch(fanMode){
-      case(FANMODE.NOCONTROL):{
+    switch (fanMode) {
+      case FANMODE.NOCONTROL: {
         break;
       }
-      case(FANMODE.FIX):{
+      case FANMODE.FIX: {
         var fixSpeed = fanSetting?.fixSpeed;
-        FanControl.setPoint.temperature=FanControl.nowPoint.temperature;
-        FanControl.setPoint.fanRPMpercent=fixSpeed;
+        FanControl.setPoint.temperature = FanControl.nowPoint.temperature;
+        FanControl.setPoint.fanRPMpercent = fixSpeed;
         break;
       }
-      case(FANMODE.CURVE):{
-        var curvePoints = fanSetting?.curvePoints!!.sort((a:fanPosition,b:fanPosition)=>{
-          return a.temperature==b.temperature?a.fanRPMpercent!!-b.fanRPMpercent!!:a.temperature!!-b.temperature!!
-        });
+      case FANMODE.CURVE: {
+        var curvePoints = fanSetting?.curvePoints!!.sort(
+          (a: fanPosition, b: fanPosition) => {
+            return a.temperature == b.temperature
+              ? a.fanRPMpercent!! - b.fanRPMpercent!!
+              : a.temperature!! - b.temperature!!;
+          }
+        );
         //每俩点判断是否在这俩点之间
-        var lineStart = new fanPosition(fanPosition.tempMin,fanPosition.fanMin);
-        if(curvePoints?.length!!>0){
+        var lineStart = new fanPosition(
+          fanPosition.tempMin,
+          fanPosition.fanMin
+        );
+        if (curvePoints?.length!! > 0) {
           //初始点到第一个点
           var lineEnd = curvePoints!![0];
-          if(FanControl.nowPoint.temperature!!>lineStart.temperature!!&&FanControl.nowPoint.temperature!!<=lineEnd.temperature!!){
-            FanControl.setPoint = calPointInLine(lineStart,lineEnd,FanControl.nowPoint.temperature!!)!!;
+          if (
+            FanControl.nowPoint.temperature!! > lineStart.temperature!! &&
+            FanControl.nowPoint.temperature!! <= lineEnd.temperature!!
+          ) {
+            FanControl.setPoint = calPointInLine(
+              lineStart,
+              lineEnd,
+              FanControl.nowPoint.temperature!!
+            )!!;
           }
 
-          curvePoints?.forEach((value,index)=>{
-            if(index>curvePoints?.length!!-1)
-              return;
+          curvePoints?.forEach((value, index) => {
+            if (index > curvePoints?.length!! - 1) return;
             lineStart = value;
-            lineEnd = index == curvePoints?.length!!-1?new fanPosition(fanPosition.tempMax,fanPosition.fanMax):curvePoints!![index+1];
-            if(FanControl.nowPoint.temperature!!>lineStart.temperature!!&&FanControl.nowPoint.temperature!!<=lineEnd.temperature!!){
-              FanControl.setPoint = calPointInLine(lineStart,lineEnd,FanControl.nowPoint.temperature!!)!!;
+            lineEnd =
+              index == curvePoints?.length!! - 1
+                ? new fanPosition(fanPosition.tempMax, fanPosition.fanMax)
+                : curvePoints!![index + 1];
+            if (
+              FanControl.nowPoint.temperature!! > lineStart.temperature!! &&
+              FanControl.nowPoint.temperature!! <= lineEnd.temperature!!
+            ) {
+              FanControl.setPoint = calPointInLine(
+                lineStart,
+                lineEnd,
+                FanControl.nowPoint.temperature!!
+              )!!;
               return;
             }
-          })
-        }else{
-          var lineEnd = new fanPosition(fanPosition.tempMax,fanPosition.fanMax);
-          if(FanControl.nowPoint.temperature!!>lineStart.temperature!!&&FanControl.nowPoint.temperature!!<=lineEnd.temperature!!){
-            FanControl.setPoint = calPointInLine(lineStart,lineEnd,FanControl.nowPoint.temperature!!)!!;
+          });
+        } else {
+          var lineEnd = new fanPosition(
+            fanPosition.tempMax,
+            fanPosition.fanMax
+          );
+          if (
+            FanControl.nowPoint.temperature!! > lineStart.temperature!! &&
+            FanControl.nowPoint.temperature!! <= lineEnd.temperature!!
+          ) {
+            FanControl.setPoint = calPointInLine(
+              lineStart,
+              lineEnd,
+              FanControl.nowPoint.temperature!!
+            )!!;
             break;
           }
         }
         break;
       }
-      default:{
-        console.error(`错误的fanmode = ${fanMode}`)
+      default: {
+        console.error(`错误的fanmode = ${fanMode}`);
       }
     }
     Backend.applySettings(APPLYTYPE.SET_FANRPM);
   }
 
-  static enableFan(){
-    this.fanIsEnable=true;
+  static enableFan() {
+    this.fanIsEnable = true;
     this.register();
   }
 
-  static disableFan(){
-    this.fanIsEnable=false;
+  static disableFan() {
+    this.fanIsEnable = false;
     this.unregister();
   }
 
-  static unregister(){
+  static unregister() {
     Backend.resetFanSettings();
-    if (this.intervalId != undefined)
-      clearInterval(this.intervalId);
+    if (this.intervalId != undefined) clearInterval(this.intervalId);
   }
 }
 
-
-export class PluginManager{
-  private static state:PluginState;
-  private static listeners: Map<ComponentName,Map<ComponentName,ComponentUpdateHandler>> = new Map();
-  private static suspendEndHook:any;
-  public static register = async(serverAPI:ServerAPI)=>{
-    PluginManager.state = PluginState.INIT; 
+export class PluginManager {
+  private static state: PluginState;
+  private static listeners: Map<
+    ComponentName,
+    Map<ComponentName, ComponentUpdateHandler>
+  > = new Map();
+  private static suspendEndHook: any;
+  public static register = async () => {
+    PluginManager.state = PluginState.INIT;
     await Backend.init(serverAPI);
     await localizationManager.init(serverAPI);
-    RunningApps.register();
     FanControl.register();
-    RunningApps.listenActiveChange((newAppId, oldAppId) => {
-      console.log(`newAppId=${newAppId} oldAppId=${oldAppId}`)
-      if (Settings.ensureEnable()) {
-        Backend.applySettings(APPLYTYPE.SET_ALL);
-      }
-    });
     Settings.loadSettingsFromLocalStorage();
     Backend.applySettings(APPLYTYPE.SET_ALL);
-    PluginManager.suspendEndHook = SteamClient.System.RegisterForOnResumeFromSuspend(async () => {
-      if (Settings.ensureEnable()) {
-        Backend.throwSuspendEvt()
-      }
-      Backend.applySettings(APPLYTYPE.SET_ALL);
-    });
     PluginManager.state = PluginState.RUN;
+  };
+
+  public static isIniting() {
+    return PluginManager.state == PluginState.INIT;
   }
 
-  public static isIniting()
-  {
-    return PluginManager.state==PluginState.INIT
-  }
-
-  public static unregister = ()=>{
-    PluginManager.suspendEndHook!.unregister();
+  public static unregister = () => {
     PluginManager.updateAllComponent(UpdateType.DISMOUNT);
-    RunningApps.unregister();
     FanControl.unregister();
     Backend.resetSettings();
-    PluginManager.state = PluginState.QUIT; 
-  }
+    PluginManager.state = PluginState.QUIT;
+  };
 
   //下发某个组件更新事件
-  static updateComponent(comName:ComponentName,updateType:UpdateType) {
-    if(this.listeners.has(comName)){
-      this.listeners.get(comName)?.forEach((fn,_wholisten)=>{
-        fn(comName,updateType);
+  static updateComponent(comName: ComponentName, updateType: UpdateType) {
+    if (this.listeners.has(comName)) {
+      this.listeners.get(comName)?.forEach((fn, _wholisten) => {
+        fn(comName, updateType);
         //console.log(`test_fn: wholisten=${wholisten} comName=${comName} updateType=${updateType}`);
       });
     }
   }
 
   //下发所有更新事件
-  static updateAllComponent(updateType:UpdateType) {
-    this.listeners.forEach((listenList,lisComName) => {
-      listenList.forEach((fn)=>{
+  static updateAllComponent(updateType: UpdateType) {
+    this.listeners.forEach((listenList, lisComName) => {
+      listenList.forEach((fn) => {
         fn(lisComName, updateType);
         //console.log(`test_fn:comName=${lisComName} updateType=${updateType}`);
-      })
+      });
     });
   }
 
   //监听组件更新事件（哪个组件监听，被监听组件，监听事件）
-  static listenUpdateComponent(whoListen:ComponentName,lisComponentNames:ComponentName[],lisfn: ComponentUpdateHandler) {
-    lisComponentNames.forEach((lisComponentName)=>{
-      if(this.listeners.has(lisComponentName)&&this.listeners.get(lisComponentName)!=undefined){
-        this.listeners.get(lisComponentName)?.set(whoListen,lisfn);
-      }else{
-        this.listeners.set(lisComponentName,new Map());
-        this.listeners.get(lisComponentName)?.set(whoListen,lisfn);
+  static listenUpdateComponent(
+    whoListen: ComponentName,
+    lisComponentNames: ComponentName[],
+    lisfn: ComponentUpdateHandler
+  ) {
+    lisComponentNames.forEach((lisComponentName) => {
+      if (
+        this.listeners.has(lisComponentName) &&
+        this.listeners.get(lisComponentName) != undefined
+      ) {
+        this.listeners.get(lisComponentName)?.set(whoListen, lisfn);
+      } else {
+        this.listeners.set(lisComponentName, new Map());
+        this.listeners.get(lisComponentName)?.set(whoListen, lisfn);
       }
-    })
-    
+    });
   }
 }
-
