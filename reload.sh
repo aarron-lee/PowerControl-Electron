a9f640a4e41aa61a26d4d2adfeff5518df55d9c1
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-07 12:20:14 -0500

    delete unnecessary files

--- reload.sh
+++
@@ -1,5 +0,0 @@
-cd $HOME/Development/PowerControl
-pnpm build
-sudo rm -rf ~/homebrew/plugins/PowerControl/
-sudo cp -rf $HOME/Development/PowerControl/ ~/homebrew/plugins/PowerControl/
-sudo systemctl restart plugin_loader.service
