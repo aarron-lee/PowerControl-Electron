51f4df1f9c898521dc23223d5b3f75a5899915c9
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-07 19:29:37 -0500

    english localization only (for now)

--- src/i18n/localization.ts
+++ src/i18n/localization.ts
@@ -1,17 +1,17 @@
 import { localizeMap, localizeStrEnum } from "./localizeMap";
-import serverAPI from "../util/serverApi";
+// import serverAPI from "../util/serverApi";
 
 export class localizationManager {
   private static language = "english";
   //private has_language  = false
   public static async init() {
-    await serverAPI!.callPluginMethod("get_language", {}).then((res) => {
-      if (res.success) {
-        //console.log("language = " + res.result);
-        this.language = res.result;
-        //this.has_language = true;
-      }
-    });
+    // await serverAPI!.callPluginMethod("get_language", {}).then((res) => {
+    //   if (res.success) {
+    //     //console.log("language = " + res.result);
+    //     this.language = res.result;
+    //     //this.has_language = true;
+    //   }
+    // });
   }
   public static getString(defaultString: localizeStrEnum) {
     var str =
