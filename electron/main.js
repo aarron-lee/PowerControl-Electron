f844172f4963f70ddb60da35ebd17445adf06a9b
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-10 19:54:51 -0400

    add context menu

--- electron/main.js
+++ electron/main.js
@@ -1,25 +1,43 @@
-const { app, BrowserWindow, screen: electronScreen } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  screen: electronScreen,
+  Tray,
+  Menu,
+} = require("electron");
 const path = require("path");
+const { initializeSettings, IS_WINDOW_HIDDEN } = require("./settings");
+
+const { setItem, getItem, getSettings } = initializeSettings(app);
+
+let mainWindow;
+let tray;
 
 const createMainWindow = () => {
-  let mainWindow = new BrowserWindow({
+  const show = !getItem(IS_WINDOW_HIDDEN);
+
+  mainWindow = new BrowserWindow({
     width: 400,
     height: 720,
-    show: false,
+    show,
     backgroundColor: "white",
     webPreferences: {
       nodeIntegration: false,
       webSecurity: false,
     },
   });
-  const startURL = `file://${path.join(
-    __dirname,
-    "./static/build/index.html"
-  )}`;
+  // const startURL = `file://${path.join(
+  //   __dirname,
+  //   "./static/build/index.html"
+  // )}`;
 
-  mainWindow.loadURL(startURL);
+  mainWindow.setMenuBarVisibility(false);
 
-  mainWindow.once("ready-to-show", () => mainWindow.show());
+  mainWindow.loadFile("./static/build/index.html");
+
+  createTray();
+
+  // mainWindow.once("ready-to-show", () => {mainWindow.show()});
 
   mainWindow.on("closed", () => {
     mainWindow = null;
@@ -41,3 +59,32 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, "./favicon/favicon-32x32.png"));
+
+  const contextMenu = createContextMenu();
+
+  tray?.setToolTip("PowerControl Electron");
+  tray?.setContextMenu(contextMenu);
+}
+
+function createContextMenu() {
+  const toggleWindow = () => {
+    const windowIsVisible = mainWindow.isVisible();
+    if (windowIsVisible) {
+      mainWindow.hide();
+      setItem(IS_WINDOW_HIDDEN, true);
+    } else {
+      mainWindow.show();
+      setItem(IS_WINDOW_HIDDEN, false);
+    }
+  };
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Toggle Window", click: toggleWindow },
+    { label: "Quit", click: () => app.quit() },
+  ]);
+
+  return contextMenu;
+}
